4ea226facc53835faa1b4d6cc975549eacaa0107
massica17 <1935984404@qq.com.com>
2023-08-15 09:18:50 +0800

    20230815

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,54 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                int count = 1;
-                while(true){
-                    System.out.printf("%d, ", count);
-                }
-            }
-        };
-        Thread t = new Thread(r);
-        t.start();
+    public static void main(String[] args) throws IOException {
+        List<String> list = new ArrayList<>();
+        list.add("adsfasdf");
+        System.out.println(list.toString());
+    }
+}
+
+class Student{
+    private String name;
+    private int age;
+    private double score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, int age, double score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+    public Student(){};
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
     }
 }
--- src/StreamTest.java
+++ src/StreamTest.java
@@ -1,16 +1,7 @@
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public class StreamTest {
+ class StreamTest {
     public static void main(String[] args)  throws IOException {
-        int[] a = new int[]{1, 2, 3, 4, 5, 5};
-        IntStream is = Arrays.stream(a).distinct();
-        System.out.println(is.count());
+
     }
 }
