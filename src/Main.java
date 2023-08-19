6eab5f2a0564a39546ca968b6a04b96feef6916e
massica17 <1935984404@qq.com.com>
2023-08-20 00:08:52 +0800

    20230820

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args){
+        Thread t = new Thread(() -> {
+            int n = 0;
+            for (int i = 0; i < 100; i++) {
+                System.out.println(i);
+            }
+        });
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
 
     }
 }
